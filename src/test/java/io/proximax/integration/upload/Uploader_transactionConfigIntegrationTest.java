45fdc779217607362464f25c86ebc29e60804a99
Tyrone <avanaur@gmail.com>
2018-09-14 11:53:03 +1000

    Added validation of transaction recipient on integration tests

--- src/test/java/io/proximax/integration/upload/Uploader_transactionConfigIntegrationTest.java
+++ src/test/java/io/proximax/integration/upload/Uploader_transactionConfigIntegrationTest.java
@@ -1,5 +1,11 @@
 package io.proximax.integration.upload;
 
+import io.nem.sdk.infrastructure.Listener;
+import io.nem.sdk.model.account.Account;
+import io.nem.sdk.model.blockchain.NetworkType;
+import io.nem.sdk.model.transaction.Transaction;
+import io.nem.sdk.model.transaction.TransactionInfo;
+import io.nem.sdk.model.transaction.TransferTransaction;
 import io.proximax.connection.BlockchainNetworkConnection;
 import io.proximax.connection.ConnectionConfig;
 import io.proximax.connection.IpfsConnection;
@@ -13,6 +19,7 @@ import org.junit.Test;
 
 import static io.proximax.testsupport.Constants.TEST_STRING;
 import static io.proximax.testsupport.TestDataRepository.logAndSaveResult;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -30,6 +37,23 @@ public class Uploader_transactionConfigIntegrationTest {
 	}
 
 	@Test
+	public void shouldUploadWithSignerAsRecipientByDefault() throws Exception {
+		final UploadParameter param = UploadParameter
+				.createForStringUpload(TEST_STRING, IntegrationTestProperties.getPrivateKey1())
+				.build();
+
+		final UploadResult result = unitUnderTest.upload(param);
+
+		assertThat(result, is(notNullValue()));
+		assertThat(result.getTransactionHash(), is(notNullValue()));
+		final Transaction transaction = waitForTransactionConfirmation(IntegrationTestProperties.getPrivateKey1(), result.getTransactionHash());
+		assertThat(transaction, is(instanceOf(TransferTransaction.class)));
+		assertThat(((TransferTransaction)transaction).getRecipient().plain(), is(IntegrationTestProperties.getAddress1()));
+
+		logAndSaveResult(result, getClass().getSimpleName() + ".shouldUploadWithRecipientPublicKeyProvided");
+	}
+
+	@Test
 	public void shouldUploadWithRecipientPublicKeyProvided() throws Exception {
 		final UploadParameter param = UploadParameter
 				.createForStringUpload(TEST_STRING, IntegrationTestProperties.getPrivateKey1())
@@ -40,6 +64,9 @@ public class Uploader_transactionConfigIntegrationTest {
 
 		assertThat(result, is(notNullValue()));
 		assertThat(result.getTransactionHash(), is(notNullValue()));
+		final Transaction transaction = waitForTransactionConfirmation(IntegrationTestProperties.getPrivateKey1(), result.getTransactionHash());
+		assertThat(transaction, is(instanceOf(TransferTransaction.class)));
+		assertThat(((TransferTransaction)transaction).getRecipient().plain(), is(IntegrationTestProperties.getAddress2()));
 
 		logAndSaveResult(result, getClass().getSimpleName() + ".shouldUploadWithRecipientPublicKeyProvided");
 	}
@@ -55,6 +82,9 @@ public class Uploader_transactionConfigIntegrationTest {
 
 		assertThat(result, is(notNullValue()));
 		assertThat(result.getTransactionHash(), is(notNullValue()));
+		final Transaction transaction = waitForTransactionConfirmation(IntegrationTestProperties.getPrivateKey1(), result.getTransactionHash());
+		assertThat(transaction, is(instanceOf(TransferTransaction.class)));
+		assertThat(((TransferTransaction)transaction).getRecipient().plain(), is(IntegrationTestProperties.getAddress2()));
 
 		logAndSaveResult(result, getClass().getSimpleName() + ".shouldUploadWithRecipientAddressProvided");
 	}
@@ -89,4 +119,20 @@ public class Uploader_transactionConfigIntegrationTest {
 		logAndSaveResult(result, getClass().getSimpleName() + ".shouldUploadWithUseBlockchainSecureMessageProvided");
 	}
 
+	private Transaction waitForTransactionConfirmation(String senderPrivateKey, String transactionHash) {
+		try {
+			final Listener listener = new Listener(IntegrationTestProperties.getBlockchainRestUrl());
+			listener.open().get();
+			final Transaction transaction = listener.confirmed(Account.createFromPrivateKey(senderPrivateKey, NetworkType.MIJIN_TEST).getAddress())
+					.filter(unconfirmedTxn ->
+							unconfirmedTxn.getTransactionInfo()
+									.flatMap(TransactionInfo::getHash)
+									.map(hash -> hash.equals(transactionHash))
+									.orElse(false)).blockingFirst();
+			listener.close();
+			return transaction;
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to listen on confirmed transaction", e);
+		}
+	}
 }
